b25af44d146143904e677f07f00cc76cb6382d5b
Nino Roldan <nino_roldan@yahoo.com>
2017-01-26 15:28:42 +0900

    first commit

---
+++ blocks_in_the_wild.rb
@@ -0,0 +1,15 @@
+nenu_array = [1,2,4]
+
+
+#the example below swould add the '--' per item
+#nenu_array.each {|item| print "--#{item}"}
+
+#the example below would select any items that are bigger than 2
+#print nenu_array.select {|item| item > 2}
+
+nenu_hash = {
+	'name' => 'Nenu',
+	'location' => 'Japan',
+	
+
+}
