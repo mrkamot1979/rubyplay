401412477f7466cf021eb038592b4a937a670693
Nino Roldan <nino_roldan@yahoo.com>
2017-02-08 11:48:21 +0900

    Ccompleted address book

--- Address.rb
+++ Address.rb
@@ -1,21 +1,33 @@
 class Address
-	attr_writer :kind, :street_1, :street_2, :city, :state, :postal_code
+  attr_accessor :kind, :street_1, :street_2, :city, :state, :postal_code
 
-
-	def to_s(format = 'short')
-		address = ''
-		case format
-			when 'long'
-				address += street_1 + "\n"
-				address += street_2 + "\n" if !street_2.nil?
-				address += ""	if street_2.nil?
-
-
-		end
-
-
-	end
-
-
-
+  def to_s(format = 'short')
+    address = ''
+    case format
+    when 'long'
+      address += street_1 + "\n"
+      address += street_2 + "\n" if !street_2.nil?
+      address += "#{city}, #{state} #{postal_code}"
+    when 'short'
+      address += "#{kind}: "
+      address += street_1
+      if street_2
+        address += " " + street_2
+      end
+      address += ", #{city}, #{state}, #{postal_code}"
+    end
+    address
+  end
 end
+
+home = Address.new
+home.kind = "Home"
+home.street_1 = "123 Main St."
+home.street_2 = "Mindanao Avenue"
+home.city = "Portland"
+home.state = "OR"
+home.postal_code = "12345"
+
+puts home.to_s('short')
+puts "\n"
+puts home.to_s('long')
