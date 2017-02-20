e2dd59854f53250d664ea72320d261b3b5f57f26
Nino Roldan <nino_roldan@yahoo.com>
2017-02-20 17:44:04 +0900

    updated with push to contacts

--- Address_book.rb
+++ Address_book.rb
@@ -1,13 +1,40 @@
 require "./Contact.rb"
 
 class AddressBook
+	attr_accessor :contacts
 
-	def initializer 
-		@Contacts = []
+	def initialize
+		@contacts = []
+	end
+
+
+	def add_to_address_book
+
 	end
 
 	
-	
 
-	
+	def print_contact_list
+		puts "Contact List"
+		contacts.each {
+			|contact| puts contact.to_s('last_first')
+		}
+		
+	end
+
 end
+
+
+addressbook = AddressBook.new
+
+ronin = Contact.new
+ronin.first_name = "Ronin"
+ronin.middle_name = "Abantao"
+ronin.last_name = "Roldan"
+
+ronin.add_phone_number("home", "4551549")
+
+addressbook.contacts.push(ronin)
+
+addressbook.print_contact_list
+
